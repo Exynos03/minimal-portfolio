19c449427b01895a453c099fdc9b9451d052f8f3
Exynos03 <sumitghosh345@gmail.com>
2024-11-05 15:17:49 +0530

    final touch needed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,32 +4,14 @@ export default function Footer() {
   return (
     <section>
       <div className="flex flex-col gap-4 lg:px-6 mt-16">
-        <p className="text-sm text-start text-muted-foreground">
-          Based on{" "}
+        <p className="text-sm text-center text-foreground">
+          {/* Inspired by{" "}
           <a className="text-foreground" href="https://brittanychiang.com">
             Brittany Chiang&#39;s website
           </a>{" "}
-          (they awesome). Coded in{" "}
-          <a className="text-foreground" href="https://code.visualstudio.com/">
-            Visual Studio Code.
-          </a>{" "}
-          Built with{" "}
-          <a className="text-foreground" href="https://nextjs.org/">
-            Next.js
-          </a>
-          ,{" "}
-          <a className="text-foreground" href="https://tailwindcss.com/">
-            Tailwind CSS
-          </a>{" "}
-          and{" "}
-          <a className="text-foreground" href="https://ui.shadcn.com/">
-            Shadcn/ui
-          </a>
-          , deployed with{" "}
-          <a className="text-foreground" href="https://vercel.com/">
-            Vercel
-          </a>
-          .
+          (they awesome).  */}
+          Crafted with ❤️ By Sumit Ghosh{" "}
+          
         </p>
       </div>
     </section>
